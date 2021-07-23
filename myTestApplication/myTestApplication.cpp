373ec9e459ee253d3f3e4469edc70485aa54ea44
Nafis Muktashid Arpon <arponbolche@gmail.com>
2021-07-23 15:39:16 +0600

    Update myTestApplication.cpp

--- myTestApplication/myTestApplication.cpp
+++ myTestApplication/myTestApplication.cpp
@@ -5,12 +5,12 @@ int main()
 {
     float a, b, c, d, e, f;
 
-    cout << "Name the buying price :    ";
+    cout << "Name the big number :    ";
     cin >> a;
-    cout << "Name the regular price :    ";
+    cout << "Name the small number :    ";
     cin >> b;
 
     c = a - b; 
 
-    cout <<  (c * 100) / a  << " % price increased.\n ";
+    cout <<  (c * 100) / a  << " % increased.\n ";
 }
